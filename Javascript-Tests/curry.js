22221230342c836d7c8b0e36aad285ff5b506db5
muakasan <muakasan@gmail.com>
2016-01-04 09:49:03 -0500

    changed curry demo

--- Javascript-Tests/curry.js
+++ Javascript-Tests/curry.js
@@ -1,8 +1,8 @@
 function ellipsoid(a, b, c){
-	return function ellipsoidHelper(u,v) {
+	return function(u,v) {
 	    var x = a*Math.sin(u)*Math.cos(v)*100;
 	    var y = b*Math.sin(u)*Math.sin(v)*100;
 	    var z = c*Math.cos(u)*100;
 	    return new Vector3(x,y,z);
-	}
-}
+	};
+};
